7d190592a63e075e6552ab0fd4ac41528c844a30
CSharpFri <CSharpFri@CLASS-2-12>
2017-11-24 21:19:31 +0100

    CSharpSyntax

---
+++ CSharpSyntax/Kalkulator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSyntax
+{
+    class Kalkulator
+    {
+        public static int Scitaj (int a, int b = 4)
+        {
+            return a + b;
+        }
+
+        public static int Scitaj(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public static int Scitaj(string a, string b)
+        {
+            //return int.Parse(a) + int.Parse(b);
+            int mv1 = int.Parse(a);
+            int mv2 = int.Parse(b);
+            return Scitaj(mv1 + mv2);
+        }
+
+        internal static int Vynasob(int v1, int v2, int v3, int v4, int v5)
+        {
+            return v1 * v2 * v3 * v4 * v5;
+        }
+    }
+}
--- CSharpSyntax/Program.cs
+++ CSharpSyntax/Program.cs
@@ -8,8 +8,95 @@ namespace CSharpSyntax
 {
     class Program
     {
+        public int globalnaPremenna = 7;
+
+        void TestGlobalnejPremennej()
+        {
+            int lokalnaPremenna = 9;
+            globalnaPremenna++;
+        }
+
         static void Main(string[] args)
         {
+            const int mojaKonstanta=4;
+            int polomer = 6;
+            //polomer += 1; // da sa aj napr += 8
+            //polomer++;  // najskor vyhodnoti polomer a potom +1
+            /*++polomer; */ // najskor zvysi o 1 a potom vyhodnoti polomer
+
+            
+
+
+            Console.WriteLine("Povodny polomer {0}",polomer);
+            int novyPolomer = polomer++;
+            Console.WriteLine("Novy polomer ++ {0}", novyPolomer); //najskor chyti polomer,vypise a potom priradi +1
+            int zasNovyPolomer = ++polomer;
+            Console.WriteLine("++ Zasa novy polomer {0}", zasNovyPolomer);
+
+            //Pouzitie Kalkulatora
+
+            Kalkulator.Scitaj(b:"4",a: "5");
+            Kalkulator.Scitaj(4);
+            int vysledok = Kalkulator.Vynasob(4, 6, 8, 10, 15);
+
+            if (vysledok == 4) vysledok++;
+            else
+                --vysledok;
+
+            if (vysledok == 4 )
+            {
+                vysledok++;
+                Kalkulator.Vynasob(4, 6, 8, 10, 15);
+            }
+            else if (vysledok == 5)
+            {
+                vysledok--;
+                Kalkulator.Vynasob(4, 6, 8, 10, 15);
+            }
+            else
+            {
+                //nic
+            }
+
+            switch (vysledok)
+            {
+                case 2: Console.WriteLine("Vysledok bol 2"); break;
+                case 3: Console.WriteLine("Vysledok bol 3"); break;
+                case 4:
+                case 5: Console.WriteLine("Vysledok bol 4 al 5"); break;
+                default: Console.WriteLine("Vysledok nebol 2-5");break;
+
+                
+                    
+            }
+
+            bool anoNe = false;
+            if (anoNe)
+            {
+                //nevykona sa 
+            }
+
+            if (!anoNe)
+            {
+                //vykona sa
+            }
+
+            if (vysledok==4 || vysledok ==5) //alebo
+            {
+
+            }
+
+            if (vysledok==4 && anoNe==true) // a zaroven 
+            {
+
+            }
+
+            if (((vysledok>=10 && vysledok<=20) || (vysledok>=40 && vysledok<=60)) && (vysledok !=16)) //&& medzi cislami
+            {
+
+            }
+
+            Console.ReadLine();
         }
     }
 }
